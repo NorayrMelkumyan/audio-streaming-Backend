63508de7ea94cba7937bc1ee5ddc2b74c6062400
Shavarsh Shahoyan <shavarsh.sh@gmail.com>
2022-05-05 19:02:59 +0400

    initial commit

---
+++ AudioStreamingWithContext/src/main/java/com/example/AudioStreamingWithContext/AudioStreamingWithContextApplication.java
@@ -0,0 +1,13 @@
+package com.example.AudioStreamingWithContext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudioStreamingWithContextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudioStreamingWithContextApplication.class, args);
+	}
+
+}
---
+++ AudioStreamingWithContext/src/test/java/com/example/AudioStreamingWithContext/AudioStreamingWithContextApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AudioStreamingWithContext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudioStreamingWithContextApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
